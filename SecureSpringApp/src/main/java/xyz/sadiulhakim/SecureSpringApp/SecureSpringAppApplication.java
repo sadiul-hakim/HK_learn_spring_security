504b1f3c8e47f639a71d87800bfb468cf6530d9b
Sadiul Hakim <sadiulhakim@gmail.com>
2025-03-18 19:39:14 +0600

    Documenting Spring Security

---
+++ SecureSpringApp/src/main/java/xyz/sadiulhakim/SecureSpringApp/SecureSpringAppApplication.java
@@ -0,0 +1,13 @@
+package xyz.sadiulhakim.SecureSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureSpringAppApplication.class, args);
+	}
+
+}
---
+++ SecureSpringApp/src/test/java/xyz/sadiulhakim/SecureSpringApp/SecureSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sadiulhakim.SecureSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
