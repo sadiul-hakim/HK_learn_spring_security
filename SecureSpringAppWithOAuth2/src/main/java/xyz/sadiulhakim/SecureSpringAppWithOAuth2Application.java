05b6fd2083b757de4200381e020107e48995841b
Sadiul Hakim <sadiulhakim@gmail.com>
2025-03-19 10:45:34 +0600

    Secure Spring App With OAuth2

---
+++ SecureSpringAppWithOAuth2/src/main/java/xyz/sadiulhakim/SecureSpringAppWithOAuth2Application.java
@@ -0,0 +1,13 @@
+package xyz.sadiulhakim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSpringAppWithOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSpringAppWithOAuth2Application.class, args);
+    }
+
+}
